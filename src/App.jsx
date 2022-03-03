1acbabfebd9a02f6fc669ce9a8140791475c16a1
dchyii <hongyichong@gmail.com>
2022-03-03 16:53:26 +0800

    changed to tailwindcss framework

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,29 @@
 import "./App.css";
 import { useEffect } from "react";
 import axios from "axios";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
   const API_ENTRY = import.meta.env.VITE_API_ENTRY;
 
-  useEffect(() => {
-    const fetch = async () => {
-      const header = {
-        "Access-Control-Allow-Origin": "*",
-        "Content-Type": "application/json",
-      };
-      const fetchedData = await axios.get(`${API_ENTRY}/api/cases/`, {
-        header,
-      });
-      console.log(fetchedData);
-    };
-    fetch();
-  }, []);
+  // useEffect(() => {
+  //   const fetch = async () => {
+  //     const header = {
+  //       "Access-Control-Allow-Origin": "*",
+  //       "Content-Type": "application/json",
+  //     };
+  //     const fetchedData = await axios.get(`${API_ENTRY}/api/cases/`, {
+  //       header,
+  //     });
+  //     console.log(fetchedData);
+  //   };
+  //   fetch();
+  // }, []);
 
   return (
     <div className="App">
       <h1>Hello World</h1>
+      <Dashboard />
     </div>
   );
 }
